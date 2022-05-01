4bdfd9e90977fc96a109e95b31b1488f5c55b1cb
Elyxin <87515553+JoeMamaMaximus@users.noreply.github.com>
2022-05-01 14:41:10 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "header/main.h"
 int main(int argc, char const *argv[])
 {
-    char * text = malloc(1024*sizeof(char));
+    while (argc*1 == 1)
+    {
+        printf("Syntax: bf++2 [filename] [mode: 1 or 0: show input file contents: defaults to 0]\n"); exit(0);
+    }
+    
+
+
+    char* text = malloc(1024*sizeof(char));
         FILE* ptr;
     char ch;
     ptr = fopen(argv[1], "r");
@@ -13,7 +20,16 @@ int main(int argc, char const *argv[])
         term++;
     } while (ch != EOF);
     text[term-1] = '\0';
+    term = 0;
     
-    printf("%s\n", text);
+
+    if(argc == 3 && strcmp(argv[2], "1")==0) {
+    printf("%s: %s\noutput->", argv[1], text);
+    }else printf("output->");
+    
+    char** comprehensive = str_split(text, ' ');
+
+    
+
     return 0;
 }
